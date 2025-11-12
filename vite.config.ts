1f2fd711e952857ccddc47593b513725c4dcb54f
Isildur Dúnedain <isildus@mindef.nl>
2025-11-12 11:57:44 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,3 @@
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-// import topLevelAwait from "vite-plugin-top-level-await";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(async () => ({
-//     plugins: [react(), topLevelAwait()],
-
-    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-    //
-    // 1. prevent vite from obscuring rust errors
-    //   clearScreen: false,
-    //   // 2. tauri expects a fixed port, fail if that port is not available
-    //   server: {
-    //     port: 1420,
-    //     strictPort: true,
-    //     watch: {
-    //       // 3. tell vite to ignore watching `src-tauri`
-    //       ignored: ["**/src-tauri/**"],
-    //     },
-    //   },
-// }));
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
@@ -38,7 +15,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "MyReactLibrary",
+      name: "aurora",
       fileName: (format) => `index.${format}.js`,
       formats: ["es", "cjs", "umd"],
     },
